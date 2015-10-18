0012299b698777c8e4dcf93adc8b3090952a132e
Brandon Billstrand <bbillstr@pal-nat186-53-184.itap.purdue.edu>
2015-10-17 22:32:32 -0400

    Commit 1 File

--- LUKE FIEND/ViewController.swift
+++ LUKE FIEND/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBAction func myButton(sender: AnyObject) {
+        let array = ["India Mahal", "Varsity Clubhouse", "China 1", "Scotty's Brewhouse", "Egyptian Cafe & Hooka Bar", "Papa Johns - Beck Ln", "Chumley's", "Chumley's"]
+        let randomIndex = Int(arc4random_uniform(UInt32(array.count)))
+        myLabel.text = array[randomIndex]
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
